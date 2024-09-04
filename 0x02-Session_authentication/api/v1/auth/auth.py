fbd7b9dce1b340ae1e81a6ea792430084f4dc31f
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-09-05 01:16:28 +0300

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """ Auth Class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        ''' checks if a path requires authentication
+        '''
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        ''' returns None
+        '''
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' returns None
+        '''
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+from .auth import Auth
+import base64
+from models.user import User
+from typing import Tuple, TypeVar
+
+
+class BasicAuth(Auth):
+    """Basic authentication class.
+    """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''Returns the Base64 of the Authorization header
+        '''
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.startswith('Basic '):
+            return authorization_header[6:]
+        else:
+            return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        '''Returns the decoded value of a Base64 string
+        '''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+                )
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        '''Returns the user info from the Base64 decoded value
+        '''
+        d = decoded_base64_authorization_header
+        if d is None or not isinstance(d, str):
+            return (None, None)
+        if ':' in d:
+            user_info = d.split(':', 1)
+            return (user_info[0], user_info[1])
+        else:
+            return (None, None)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """Retrieves a user based on the user's authentication credentials.
+        """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
