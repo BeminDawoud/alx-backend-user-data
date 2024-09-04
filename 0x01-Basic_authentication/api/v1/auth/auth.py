0914ce14d44cfb43b912d9812ffbee49a9680dac
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-09-04 04:09:45 +0300

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,9 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         ''' returns None
         '''
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         ''' returns None
