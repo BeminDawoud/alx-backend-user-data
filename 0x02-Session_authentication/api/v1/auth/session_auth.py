b569849fb1e66ba5ee4bd29c8b0036b52d334bc6
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-09-05 12:40:11 +0300

    task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,13 +7,12 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Basic authentication class.
-    """
+    """Basic authentication class."""
+
     user_id_by_session_id = dict()
 
     def create_session(self, user_id: str = None) -> str:
-        '''Creates a Session ID for a user_id
-        '''
+        """Creates a Session ID for a user_id"""
         if user_id is None:
             return None
         if not isinstance(user_id, str):
@@ -23,8 +22,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        '''Returns a User ID based on a Session ID
-        '''
+        """Returns a User ID based on a Session ID"""
         if session_id is None:
             return None
         if not isinstance(session_id, str):
@@ -32,8 +30,17 @@ class SessionAuth(Auth):
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        '''Returns a User instance based on a cookie value
-        '''
+        """Returns a User instance based on a cookie value"""
         session_id = self.session_cookie(request)
         user_id = self.user_id_by_session_id.get(session_id)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Destroys an authenticated session."""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if (request is None or session_id is None) or user_id is None:
+            return False
+        if session_id in self.user_id_by_session_id:
+            del self.user_id_by_session_id[session_id]
+        return True
