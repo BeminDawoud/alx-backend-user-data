eba869b48fa6737564b0e68a5e44a43f432f6b22
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-09-05 06:53:23 +0300

    missig request from flask

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,31 +2,32 @@
 """ Module of session views
 """
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 from models.user import User
 import os
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def login():
-    """ POST /api/v1//auth_session/login
+    """POST /api/v1//auth_session/login
     Return:
       - Dictionary representation of the User
     """
-    email = request.form.get('email')
-    password = request.form.get('password')
+    email = request.form.get("email")
+    password = request.form.get("password")
     if not email:
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({'email': email})
+    users = User.search({"email": email})
     if not users or users[0]:
         return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
+
     session_id = auth.create_session(user.id)
     res = jsonify(user.to_json())
-    res.set_cookie(os.getenv("SESSION_NAME"), sessiond_id)
+    res.set_cookie(os.getenv("SESSION_NAME"), session_id)
     return res
