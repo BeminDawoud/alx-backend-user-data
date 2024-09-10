4af913cf5d337ca13731cd0fc34fe0d130699322
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-09-10 16:06:52 +0300

    Task 8 validate_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """auth module
 """
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -20,6 +20,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """Registers a new user"""
         try:
             if self._db.find_user_by(email=email):
                 raise ValueError(f"User {email} already exists")
@@ -27,3 +28,14 @@ class Auth:
             return self._db.add_user(
                 email=email, hashed_password=_hash_password(password)
             )
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates the user login"""
+        user = self._db.find_user_by(email=email)
+        if user:
+            if checkpw(user.hashed_password, _hash_password(password)):
+                return True
+            else:
+                return False
+        else:
+            return False
